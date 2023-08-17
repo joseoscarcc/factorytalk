0dd68928fdefcde07b1d2a93d61a68a996ee1820
soseguera <soseguera@royalasg.com>
2023-08-17 11:39:47 -0700

    Energy Branch

---
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.DataLogger;
+using FTOptix.NativeUI;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.CoreBase;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.Retentivity;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.WebUI;
+using FTOptix.Report;
+using FTOptix.Modbus;
+#endregion
+
+public class DesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,32 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.DataLogger;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.SQLiteStore;
+using FTOptix.CoreBase;
+using FTOptix.Store;
+using FTOptix.Retentivity;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.Report;
+using FTOptix.Modbus;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
